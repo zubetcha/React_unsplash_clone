e8e4cc93313db65ade655e22bdb8f0cb0d8b72fc
zubetcha <zuhye5@gmail.com>
2021-12-14 18:03:59 +0900

    [주혜] Detial page view completed, Upload page view start

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -2,51 +2,66 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Grid = (props) => {
+  const { position, children, flex, align, width, padding, margin, _onClick, bg, height, overflowY, verticalAlign, alignItems, maxWidth } = props
+  const styles = {
+    flex: flex,
+    position: position,
+    width: width,
+    height: height,
+    padding: padding,
+    margin: margin,
+    align: align,
+    bg: bg,
 
+    // 주혜 추가
+    overflowY: overflowY,
+    verticalAlign: verticalAlign,
+    alignItems: alignItems,
+    maxWidth: maxWidth,
+  }
 
-    const {position, children, flex, align, width, padding, margin, _onClick, bg, height} =props;
-    const styles = {
-        flex:flex,
-        position: position,
-        width:width,
-        height:height,
-        padding: padding,
-        margin: margin,
-        align:align,
-        bg:bg
-
-    }
-
-    return (
-        <GridBox {...styles} onClick={_onClick}>
-            {children}
-        </GridBox>
-    );
-};
+  return (
+    <GridBox {...styles} onClick={_onClick}>
+      {children}
+    </GridBox>
+  )
+}
 
 Grid.defaultProps = {
-    position:false,
-    children:null,
-    flex:false,
-    width:"100%",
-    height:false,
-    padding:false,
-    margin:false,
-    align:false,
-    bg:false,
-    _onClick:()=>{}
+  position: false,
+  children: null,
+  flex: false,
+  width: '100%',
+  height: false,
+  padding: false,
+  margin: false,
+  align: false,
+  bg: false,
+  _onClick: () => {},
+
+  // 주혜 추가
+  overflowY: false,
+  verticalAlign: false,
+  alignItems: false,
+  maxWidth: '',
 }
 
 const GridBox = styled.div`
-    position: ${(props) => props.position ? `${props.position}`: ""};
-    width: ${(props) => props.width};
-    height:${(props) => props.height};
-    box-sizing: border-box;
-    padding:${(props)=> props.padding ? `${props.padding}`: ""};
-    margin:${(props)=> props.margin ? `${props.margin}`: ""};
-    display:${(props)=> props.flex ? `${props.flex}`: ""};
-    text-align:${(props) => props.align};
-    background:${(props) => props.bg};
+  position: ${(props) => (props.position ? `${props.position}` : '')};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  box-sizing: border-box;
+  padding: ${(props) => (props.padding ? `${props.padding}` : '')};
+  margin: ${(props) => (props.margin ? `${props.margin}` : '')};
+  display: ${(props) => (props.flex ? `${props.flex}` : '')};
+  text-align: ${(props) => props.align};
+  background: ${(props) => props.bg};
+
+  // 주혜 추가
+  overflow-y: ${(props) => props.overflowY};
+  vertical-align: ${(props) => (props.verticalAlign ? `${props.verticalAlign};` : '')};
+  align-items: ${(props) => (props.alignItems ? `${props.alignItems};` : '')};
+  max-width: ${(props) => (props.maxWidth ? `${props.maxWidth};` : '')};
 `
 
 export default Grid
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,51 +1,131 @@
 import React from 'react'
 import styled from 'styled-components'
+import './Detail.css'
 
-// elements
-import { Grid } from '../elements'
+// react-icons
+import { IoCheckmarkCircleSharp } from 'react-icons/io5'
+import { HiHeart } from 'react-icons/hi'
+import { GoPlus } from 'react-icons/go'
+import { IoIosArrowDown } from 'react-icons/io'
+import { IoArrowRedoSharp } from 'react-icons/io5'
+import { IoLocationSharp } from 'react-icons/io5'
+import { MdInfo } from 'react-icons/md'
+import { BsThreeDots } from 'react-icons/bs'
+import { FiCalendar } from 'react-icons/fi'
+import { MdOutlineDescription } from 'react-icons/md'
 
 const Detail = (props) => {
   return (
     <>
       <ModalBody>
         <ModalOverlay>
-          <LeftPanel></LeftPanel>
-          <DetailContainer>
-            <ModalHeader>
-              <UserBox>
-                <UserProfile>
-                  <img></img>
-                </UserProfile>
-                <UserInfo>
-                  <p className="username">ZUBETCHA</p>
-                  <p className="user-status">Available for hire</p>
-                </UserInfo>
-              </UserBox>
-              <Toggle>
-                <div className="icon-box">
-                  <button className="icon-btn">하트</button>
+          <ModalContent className="modal-content">
+            <div></div>
+            <DetailContainer>
+              <ModalHeader>
+                <UserBox>
+                  <UserProfile>
+                    <img src="https://images.unsplash.com/placeholder-avatars/extra-large.jpg?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff"></img>
+                  </UserProfile>
+                  <UserInfo>
+                    <p className="username">ZUBETCHA</p>
+                    <div className="user-status-flex">
+                      <p className="user-status">Available for hire</p>
+                      <IoCheckmarkCircleSharp />
+                    </div>
+                  </UserInfo>
+                </UserBox>
+                <Toggle>
+                  <div className="icon-box">
+                    <button className="icon-btn">
+                      <HiHeart />
+                    </button>
+                  </div>
+                  <div className="icon-box">
+                    <button className="icon-btn">
+                      <GoPlus />
+                    </button>
+                  </div>
+                  <DownloadToggle>
+                    <div>
+                      <button className="download-btn btn">Download free</button>
+                    </div>
+                    <div>
+                      <button className="size-btn btn">
+                        <IoIosArrowDown />
+                      </button>
+                    </div>
+                  </DownloadToggle>
+                </Toggle>
+              </ModalHeader>
+              <ModalImage>
+                <div>
+                  <div className="image-wrap">
+                    <div className="image-container">
+                      <img src="https://images.unsplash.com/photo-1639365095893-0269096a3ba3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2969&q=80"></img>
+                    </div>
+                  </div>
                 </div>
-                <div className="icon-box">
-                  <button className="icon-btn">+</button>
-                </div>
-                <DownloadToggle>
+              </ModalImage>
+              <ModalInfo>
+                <div className="image-info">
                   <div>
-                    <button className="download-btn btn">Download free</button>
+                    <h3 className="image-info-title">Views</h3>
+                    <span className="image-info-content">--</span>
                   </div>
                   <div>
-                    <button className="size-btn btn">dd</button>
+                    <h3 className="image-info-title">Downloads</h3>
+                    <span className="image-info-content">657</span>
                   </div>
-                </DownloadToggle>
-              </Toggle>
-            </ModalHeader>
-            <ModalImage>
-              <div>
-                <div></div>
-              </div>
-            </ModalImage>
-            <ModalInfo></ModalInfo>
-          </DetailContainer>
-          <RightPanel></RightPanel>
+                  <div>
+                    <h3 className="image-info-title">Featured in</h3>
+                    <span className="image-info-content">
+                      <a>Tagname</a>
+                    </span>
+                  </div>
+                </div>
+                <div className="image-toggle">
+                  <div className="icon-box">
+                    <button className="icon-btn">
+                      <IoArrowRedoSharp />
+                      <span>&nbsp;Shared</span>
+                    </button>
+                  </div>
+                  <div className="icon-box">
+                    <button className="icon-btn">
+                      <MdInfo />
+                      <span>&nbsp;Info</span>
+                    </button>
+                  </div>
+                  <div className="icon-box">
+                    <button className="icon-btn">
+                      <BsThreeDots />
+                    </button>
+                  </div>
+                </div>
+              </ModalInfo>
+              <ModalDesc>
+                <div className="image-desc">
+                  <span className="image-desc-icon">
+                    <IoLocationSharp />
+                  </span>
+                  <p className="image-desc-text">Location</p>
+                </div>
+                <div className="image-desc">
+                  <span className="image-desc-icon">
+                    <FiCalendar />
+                  </span>
+                  <p className="image-desc-text">Modified At</p>
+                </div>
+                <div className="image-desc">
+                  <span className="image-desc-icon">
+                    <MdOutlineDescription />
+                  </span>
+                  <p className="image-desc-text">Description</p>
+                </div>
+              </ModalDesc>
+            </DetailContainer>
+          </ModalContent>
         </ModalOverlay>
       </ModalBody>
     </>
@@ -59,10 +139,11 @@ const ModalBody = styled.div`
   /* opacity: 0; */
   box-sizing: border-box;
   z-index: 0;
+  overflow-y: hidden;
 `
 
 const ModalOverlay = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100%;
   box-sizing: border-box;
   backface-visibility: hidden;
@@ -74,26 +155,16 @@ const ModalOverlay = styled.div`
   bottom: 0;
   left: 0;
   z-index: 5;
+`
+
+const ModalContent = styled.div`
+  min-height: 100%;
   display: flex;
-  flex-direction: row;
-`
-
-const LeftPanel = styled.div`
-  width: 70px;
-  min-height: 100%;
-  background-color: #0009;
-`
-
-const RightPanel = styled.div`
-  width: 70px;
-  min-height: 100%;
-  background-color: #0009;
+  flex-direction: column;
 `
 
 const DetailContainer = styled.div`
   box-sizing: border-box;
-  margin: 16px 0 100px;
-  width: calc(100% - 140px);
   min-height: 100%;
   cursor: auto;
   display: flex;
@@ -101,6 +172,44 @@ const DetailContainer = styled.div`
   outline: none;
   pointer-events: none;
   background-color: #999;
+
+  .icon-box {
+    padding: 4px;
+
+    .icon-btn {
+      outline: none;
+      display: inline-flex;
+      background-color: #fff;
+      border: 0.5px solid #d1d1d1;
+      padding: 0 9px;
+      margin: 0;
+      font-size: 14px;
+      font-weight: 600;
+      text-align: center;
+      border-radius: 4px;
+      height: 32px;
+      line-height: 30px;
+      color: #767676;
+      cursor: pointer;
+      transition: all 0.1s ease-in-out;
+
+      &:hover {
+        color: #111;
+        border: 0.5px solid #111;
+
+        svg {
+          color: #111;
+        }
+      }
+
+      svg {
+        color: #767676;
+        font-size: 19px;
+        line-height: 30px;
+        padding-top: 5px;
+      }
+    }
+  }
 `
 
 const ModalHeader = styled.div`
@@ -110,7 +219,7 @@ const ModalHeader = styled.div`
   display: flex;
   align-items: center;
   width: auto;
-  padding: 9px 12px;
+  padding: 6px 12px;
   pointer-events: auto;
   cursor: auto;
   background-color: #fff;
@@ -138,7 +247,6 @@ const UserProfile = styled.div`
     width: 100%;
     height: 100%;
     border-radius: 16px;
-    background-image: url(https://images.unsplash.com/placeholder-avatars/extra-large.jpg?dpr=2&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff);
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
@@ -158,12 +266,22 @@ const UserInfo = styled.div`
     text-align: left;
   }
 
-  .user-status {
-    font-size: 12px;
-    line-height: 1.35;
+  .user-status-flex {
+    display: flex;
+    align-items: center;
     color: #007fff;
-    margin: 0;
-    text-align: left;
+
+    .user-status {
+      font-size: 12px;
+      line-height: 1.35;
+      margin: 0;
+      text-align: left;
+    }
+
+    svg {
+      font-size: 14px;
+      margin-left: 3px;
+    }
   }
 `
 
@@ -171,31 +289,6 @@ const Toggle = styled.div`
   display: flex;
   align-items: center;
   flex-direction: row;
-
-  .icon-box {
-    padding: 4px;
-
-    .icon-btn {
-      outline: none;
-      display: inline-flex;
-      background-color: #fff;
-      border: 0.5px solid #d1d1d1;
-      padding: 0 11px;
-      margin: 0;
-      font-size: 14px;
-      font-weight: 500;
-      text-align: center;
-      border-radius: 4px;
-      height: 32px;
-      line-height: 30px;
-      color: #767676;
-      cursor: pointer;
-
-      svg {
-        color: #767676;
-      }
-    }
-  }
 `
 
 const DownloadToggle = styled.div`
@@ -229,7 +322,7 @@ const DownloadToggle = styled.div`
     .download-btn {
       border-top-right-radius: 0;
       border-bottom-right-radius: 0;
-      margin-right: 0.5px;
+      margin-right: 0.7px;
     }
 
     .size-btn {
@@ -238,12 +331,102 @@ const DownloadToggle = styled.div`
 
       svg {
         color: #fff;
+        font-size: 19px;
+        line-height: 30px;
+        padding-top: 5px;
       }
     }
   }
 `
-const ModalImage = styled.div``
+const ModalImage = styled.div`
+  .image-wrap {
+    box-sizing: border-box;
+    padding: 10px 16px;
+    background-color: #fff;
 
-const ModalInfo = styled.div``
+    .image-container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      img {
+        min-width: min(100%, 872px);
+        max-width: calc((100vh - 175px) * 1.50316);
+        /* height: 100%; */
+        margin: 0 auto;
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+      }
+    }
+  }
+`
+
+const ModalInfo = styled.div`
+  padding: 14px 16px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 24px;
+  background-color: #fff;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+
+  .image-info {
+    display: grid;
+    grid-template-columns: repeat(3, minmax(auto, 200px));
+    align-items: start;
+
+    .image-info-title {
+      margin: 0;
+      color: #767676;
+      font-size: 14px;
+      width: fit-content;
+      font-weight: inherit;
+      line-height: 1.6;
+    }
+
+    .image-info-content {
+      color: #111;
+      font-size: 15px;
+      font-weight: 500;
+      line-height: 1.6;
+    }
+  }
+
+  .image-toggle {
+    display: flex;
+    justify-content: right;
+  }
+`
+
+const ModalDesc = styled.div`
+  padding: 14px 16px;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+
+  .image-desc {
+    display: flex;
+    align-items: center;
+    color: #767676;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.6;
+
+    .image-desc-icon {
+      width: 18px;
+      margin: 0 7px 0 0;
+
+      svg {
+        font-size: 16px;
+        padding-top: 5px;
+      }
+    }
+
+    .image-desc-text {
+      margin: 0;
+    }
+  }
+`
 
 export default Detail
